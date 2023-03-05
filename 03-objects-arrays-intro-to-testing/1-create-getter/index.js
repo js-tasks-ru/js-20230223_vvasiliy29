e195254ca3e96b588880755a894098900cd0ca0f
John <vasiliy.l@thrive.io>
2023-03-05 17:06:08 +0300

    implemented create getter

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,8 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
-
+  const sep = ".";
+  const props = path.split(sep);
+  const reduceCallback = (accumObj, prop) => accumObj?.[prop];
+  return (obj) => props.reduce(reduceCallback, obj);
 }
