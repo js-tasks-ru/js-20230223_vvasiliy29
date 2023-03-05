e16566140a90a97554e4c4c7283d50b7f7dec56e
John <vasiliy.l@thrive.io>
2023-03-05 18:25:40 +0300

    implemented trim symbols

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,14 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
-
+  if (!size) {
+    return size === 0 ? '' : string;
+  }
+  let currentLatter = '';
+  let count = 0;
+  return string.split('').filter((latter) => {
+    count = currentLatter !== latter ? 1 : count + 1;
+    currentLatter = latter;
+    return count <= size;
+  }).join('');
 }
